dacc20b8fbb289f476e1627da7f20d0cccc4776e
Omar Mohammad <omarb2515@gmail.com>
2026-01-09 15:10:58 +0200

    Merge pull request #2 CI pipeline setup
    CI pipeline

--- chat-server/src/main/java/com/jets/chat/server/config/DataSourceConfig.java
+++ chat-server/src/main/java/com/jets/chat/server/config/DataSourceConfig.java
@@ -1,20 +1,20 @@
 package com.jets.chat.server.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
 import java.io.IOException;
 import java.util.Properties;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class DataSourceConfig {
     private static HikariDataSource dataSource;
 
     static {
         try {
-            Properties prop =new Properties();
+            Properties prop = new Properties();
             prop.load(DataSourceConfig.class.getResourceAsStream("/db.properties"));
 
-            HikariConfig config =new HikariConfig(prop);
+            HikariConfig config = new HikariConfig(prop);
             dataSource = new HikariDataSource(config);
         } catch (IOException e) {
             System.out.println("Could establish connection with the database.");
