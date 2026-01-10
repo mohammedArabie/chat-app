5a3468a43492f3ef48f7acd7560b9b0ec66444f5
mohammedArabie <mo.arabie83@gmail.com>
2026-01-10 22:22:42 +0200

    add client and server managers to bootstrap AnnouncementService over RMI

---
+++ chat-server/src/main/java/com/jets/chat/server/Server.java
@@ -0,0 +1,37 @@
+package com.jets.chat.server;
+
+import com.jets.chat.common.rmi.RemoteAnnouncementService;
+import com.jets.chat.common.util.ProjectConstants;
+import com.jets.chat.server.context.ServerManager;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerManager serverManager = ServerManager.getInstance();
+
+            RemoteAnnouncementService remoteAnnouncementService = serverManager
+                    .getRemoteAnnouncementService();
+
+            Registry registry = LocateRegistry.createRegistry(ProjectConstants.RMI_SERVICE_PORT);
+
+            registry.rebind(ProjectConstants.ANNOUNCEMENT_SERVICE, remoteAnnouncementService);
+
+            System.out.println("Server Running");
+
+            // Needed when using exec:java to run the server
+            // Not needed when using java to run
+            Thread.currentThread().join();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+    }
+}
