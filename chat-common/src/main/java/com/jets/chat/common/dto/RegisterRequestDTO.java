9f11bdf2e1740c618a0458cbb29d70871a71731b
Rawan <rawanalmashad@gmail.com>
2026-01-23 04:53:38 +0200

    Added RegisterDtos

---
+++ chat-common/src/main/java/com/jets/chat/common/dto/RegisterRequestDTO.java
@@ -0,0 +1,83 @@
+package com.jets.chat.common.dto;
+
+import com.jets.chat.common.enums.Gender;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class RegisterRequestDTO implements Serializable {
+    private String phoneNumber;
+    private String displayName;
+    private String email;
+    private String password;
+    private Gender gender;
+    private String country;
+    private Date dateOfBirth;
+    private String bio;
+    public RegisterRequestDTO() {
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
