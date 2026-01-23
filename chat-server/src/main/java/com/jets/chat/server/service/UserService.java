26b593ad6b9477265ed723658c1082166f997b97
Rawan <rawanalmashad@gmail.com>
2026-01-23 05:00:19 +0200

    Added UserService

---
+++ chat-server/src/main/java/com/jets/chat/server/service/UserService.java
@@ -0,0 +1,9 @@
+package com.jets.chat.server.service;
+
+import com.jets.chat.common.dto.RegisterRequestDTO;
+import com.jets.chat.common.dto.RegisterResponseDTO;
+public interface UserService {
+
+    public RegisterResponseDTO register(RegisterRequestDTO dto);
+
+}
