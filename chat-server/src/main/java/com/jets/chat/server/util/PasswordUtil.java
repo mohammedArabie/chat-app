c8d3f2ed30dc86692f389fd6062c5ac5828a93a3
Rawan <rawanalmashad@gmail.com>
2026-01-24 22:20:54 +0200

    Modified PasswordUtil

--- chat-server/src/main/java/com/jets/chat/server/util/PasswordUtil.java
+++ chat-server/src/main/java/com/jets/chat/server/util/PasswordUtil.java
@@ -3,16 +3,18 @@ package com.jets.chat.server.util;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import java.nio.charset.StandardCharsets;
+
 public class PasswordUtil {
 
     private PasswordUtil() {
-        // prevent instantiation
+        // prevent creating objects
     }
 
     public static String hash(String password) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hashedBytes = md.digest(password.getBytes());
+            byte[] hashedBytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
 
             StringBuilder sb = new StringBuilder();
             for (byte b : hashedBytes) {
