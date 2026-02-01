afc7631f7378817fe1de6586890a61d21bfae7c1
OmarMUhammed03 <omarb2515@gmail.com>
2026-02-01 16:12:07 +0200

    Add chat service lookup

--- chat-client/src/main/java/com/jets/chat/client/util/ClientManager.java
+++ chat-client/src/main/java/com/jets/chat/client/util/ClientManager.java
@@ -1,6 +1,8 @@
 package com.jets.chat.client.util;
 
 import com.jets.chat.common.rmi.RemoteAnnouncementService;
+import com.jets.chat.common.rmi.RemoteChatService;
+import com.jets.chat.common.rmi.RemoteClientService;
 import com.jets.chat.common.util.ProjectConstants;
 
 import java.rmi.NotBoundException;
@@ -13,6 +15,26 @@ public class ClientManager {
 
     private final RemoteAnnouncementService remoteAnnouncementService;
 
+    private final RemoteChatService remoteChatService;
+
+    private ClientManager() {
+        try {
+            Registry registry = LocateRegistry.getRegistry(ProjectConstants.RMI_SERVICE_HOST,
+                    ProjectConstants.RMI_SERVICE_PORT);
+            this.remoteAnnouncementService = (RemoteAnnouncementService) registry
+                    .lookup(ProjectConstants.ANNOUNCEMENT_SERVICE);
+            this.remoteChatService = (RemoteChatService) registry
+                    .lookup(ProjectConstants.CHAT_SERVICE);
+
+        } catch (RemoteException e) {
+            System.out.println("Failed to connect");
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (NotBoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static ClientManager getInstance() {
         if (instance == null) {
             synchronized (ClientManager.class) {
@@ -24,23 +46,19 @@ public class ClientManager {
         return instance;
     }
 
-    private ClientManager() {
+    public void registerCallback(Long userId, RemoteClientService callback) {
         try {
-            Registry registry = LocateRegistry.getRegistry(ProjectConstants.RMI_SERVICE_HOST,
-                    ProjectConstants.RMI_SERVICE_PORT);
-            this.remoteAnnouncementService = (RemoteAnnouncementService) registry
-                    .lookup(ProjectConstants.ANNOUNCEMENT_SERVICE);
-
-        } catch (RemoteException e) {
-            System.out.println("Failed to connect");
+            remoteChatService.registerClient(userId, callback);
+        } catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException(e);
-        } catch (NotBoundException e) {
-            throw new RuntimeException(e);
         }
     }
 
     public RemoteAnnouncementService getRemoteAnnouncementService() {
         return remoteAnnouncementService;
+    }
+
+    public RemoteChatService getRemoteChatService() {
+        return remoteChatService;
     }
 }
