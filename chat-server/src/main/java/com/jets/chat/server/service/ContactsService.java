858c4491c195d8f64320fa1e6a5977bfa3b9fd5a
OmarMUhammed03 <omarb2515@gmail.com>
2026-02-04 22:16:15 +0200

    add minor changes

--- chat-server/src/main/java/com/jets/chat/server/service/ContactsService.java
+++ chat-server/src/main/java/com/jets/chat/server/service/ContactsService.java
@@ -3,6 +3,7 @@ package com.jets.chat.server.service;
 import com.jets.chat.common.dto.InvitationDTO;
 import com.jets.chat.server.entity.Contact;
 
+import java.rmi.RemoteException;
 import java.util.List;
 
 public interface ContactsService {
@@ -11,7 +12,7 @@ public interface ContactsService {
 
     void sendRequest(long fromId, long toId);
 
-    void acceptRequest(long contactId, long ownerId);
+    void acceptRequest(long contactId, long ownerId) throws RemoteException;
 
     void declineRequest(long contactId, long ownerId);
 
