91441d5d51c7406a22395c69d65b2571fbfc1ae5
OmarMUhammed03 <omarb2515@gmail.com>
2026-02-05 02:17:06 +0200

    refactor MessageDTO to rename chatID to chatId for consistency

--- chat-common/src/main/java/com/jets/chat/common/dto/MessageDTO.java
+++ chat-common/src/main/java/com/jets/chat/common/dto/MessageDTO.java
@@ -3,6 +3,6 @@ package com.jets.chat.common.dto;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public record MessageDTO(String content, LocalDateTime time, boolean isSentByMe,
-        String senderName, long chatID) implements Serializable {
+public record MessageDTO(String content, LocalDateTime time, boolean isSentByMe, String senderName,
+        long chatId) implements Serializable {
 }
