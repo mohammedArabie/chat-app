d3adcf93df0b6093c84c5d4f8558d7c22f390ceb
mohammedArabie <mo.arabie83@gmail.com>
2026-02-05 03:28:42 +0200

    make executable jars for server and client applications

---
+++ chat-server/src/main/java/com/jets/chat/server/admin/ServerMain.java
@@ -0,0 +1,8 @@
+package com.jets.chat.server.admin;
+
+public class ServerMain {
+
+    public static void main(String[] args) {
+        javafx.application.Application.launch(AdminApplication.class, args);
+    }
+}
